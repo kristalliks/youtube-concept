9a055f91f55e0b3bbfeca22987de4803bef00b7e
kristalliks <47499383+kristalliks@users.noreply.github.com>
2021-04-09 15:39:03 +0300

    Пофиксены меню и шапка, добавлен контент

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+
+const swiper3 = new Swiper('.food-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.food-button-next',
+    prevEl: '.food-button-prev',
+  },
+});
+
